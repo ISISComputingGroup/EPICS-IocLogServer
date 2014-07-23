7f4152b712bce681dfe248e020445ca3b8395d73
Kris Ward <kris.ward@stfc.ac.uk>
2014-07-23 10:13:05 +0000

    #441 - Added additional fields to log server JMS messages.
    Former-commit-id: 170667876a8432a60d2a579b94c0c8c244028368

--- base/IocLogServer/src/org/isis/logserver/xml/XmlWriter.java
+++ base/IocLogServer/src/org/isis/logserver/xml/XmlWriter.java
@@ -17,11 +17,15 @@ public class XmlWriter
 	// XML tags - do not change these without changing the corresponding
 	//	tags in the XML reader in any client code (e.g. Eclipse CSS)
 	private static final String MESSAGE_TAG = "message";
+	
 	private static final String CONTENTS_TAG = "contents";
 	private static final String SEVERITY_TAG = "severity";
 	private static final String EVENTTIME_TAG = "eventTime";
+	private static final String CREATETIME_TAG = "createTime";
 	private static final String CLIENTNAME_TAG = "clientName";
+	private static final String CLIENTHOST_TAG = "clientHost";
 	private static final String TYPE_TAG = "type";
+	private static final String APPID_TAG = "applicationId";
 	
 	/**
 	 * Convert the message in to an XML format that can be sent over JMS
@@ -35,8 +39,11 @@ public class XmlWriter
 		xml.append(makeElement(CONTENTS_TAG, message.getContents()));
 		xml.append(makeElement(SEVERITY_TAG, message.getSeverity()));
 		xml.append(makeElement(EVENTTIME_TAG, message.getEventTime()));
+		xml.append(makeElement(CREATETIME_TAG, message.getCreateTime()));
 		xml.append(makeElement(CLIENTNAME_TAG, message.getClientName()));
+		xml.append(makeElement(CLIENTHOST_TAG, message.getClientHost()));
 		xml.append(makeElement(TYPE_TAG, message.getType()));
+		xml.append(makeElement(APPID_TAG, message.getApplicationId()));
 		
 		xml.append(makeCloseTag(MESSAGE_TAG));
 		
