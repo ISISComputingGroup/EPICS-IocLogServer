76bb691081434176d7ca97e07b8e605d7f1b6836
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-06-30 12:53:40 +0100

    Removed ActiveMQ as this is now a central service

--- procserv_start.bat
+++ procserv_start.bat
@@ -12,17 +12,6 @@ IF "%ICPVARDIR%"=="" (
 set IOCLOGROOT=%ICPVARDIR%/logs/ioc
 for /F "usebackq" %%I in (`cygpath %IOCLOGROOT%`) do SET IOCCYGLOGROOT=%%I
 
-REM *****************************************
-REM *        JMS SERVER
-REM *****************************************
-set STARTCMD=%ComSpec% /c %MYDIRIOCLOG%start-jms-server.bat
-set CONSOLEPORT=9001
-set LOG_FILE=%IOCCYGLOGROOT%/JMS-%%Y%%m%%d.log
-
-@echo Starting JMS Log Server on 127.0.0.1 (console port %CONSOLEPORT%)
-@echo * log file - %LOG_FILE%
-%ICPTOOLS%\cygwin_bin\procServ.exe --logstamp --logfile="%LOG_FILE%" --timefmt="%%c" --restrict --ignore="^D^C" --name=JMS --pidfile="/cygdrive/c/windows/temp/EPICS_JMS.pid" %CONSOLEPORT% %STARTCMD%
-
 
 REM *****************************************
 REM *        LOG SERVER
