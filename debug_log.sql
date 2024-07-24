dc5eb38efba12afab6c576ffb2c6e02407911c6a
Ian Gillingham <rna55576@NDW2920.isis.cclrc.ac.uk>
2024-07-24 15:16:23 +0100

    chore: init

---
+++ debug_log.sql
@@ -0,0 +1,14 @@
+DROP PROCEDURE IF EXISTS debug_log;
+
+DELIMITER //
+CREATE PROCEDURE debug_log(IN log_message VARCHAR(255))
+	BEGIN
+	   CREATE TABLE IF NOT EXISTS debug_messages(
+	   id INT AUTO_INCREMENT PRIMARY KEY,
+	   witten_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	   debug_message VARCHAR(255)) ENGINE=INNODB;
+       
+		INSERT INTO debug_messages (debug_message) VALUES (log_message);
+	END//
+
+DELIMITER ;
